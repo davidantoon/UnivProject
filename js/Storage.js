a2a55483d0ecec17b7155316e72e71b6dc2ff5b1
davidantoon <david.antoon@hotmail.com>
2015-08-14 02:29:26 +0300

    Added new OOP functions
    Demo files:
    1) prac.html
    2) /js/app2.js

---
+++ js/Storage.js
@@ -0,0 +1,32 @@
+app.factory('Storage', function(){
+
+	/**
+	 * Used to fillter the log file
+	 * @type {String}
+	 */
+	var prefixKey = "com.intel.videoManager";
+
+	/**
+	 * Default constructor to prevent errors!
+	 */
+	function Storage(){}
+
+	// Statis method to add new data to localStorage
+	function save(key, value){
+
+	}
+
+	// Statis method to get data from localStorage
+	function get(key){
+		
+	}
+
+	// Statis method to clear localStorage's data
+	function clear(key, value){
+		
+	}
+
+
+
+	return Storage;
+});
--- js/app2.js
+++ js/app2.js
@@ -2,6 +2,15 @@ var app = angular.module('IntelLearner', ['onsen', 'firebase']);
 
 app.controller('MainCtrl', ["$scope", "Workspace", "Globals",function($scope, Workspace) {
 
-	var workspaces = new Workspace();
-	console.log(workspaces);
+	$scope.workspaces = new Workspace();
+	$scope.jsonWorkflow = JSON.parse(JSON.stringify($scope.workspaces.workflows[0].toJson()));
+	console.log($scope.workspaces);
+
+	$scope.friends =
+          [{name:'John', phone:'555-1212', age:10},
+           {name:'Mary', phone:'555-9876', age:19},
+           {name:'Mike', phone:'555-4321', age:21},
+           {name:'Adam', phone:'555-5678', age:35},
+           {name:'Julie', phone:'555-8765', age:29}];
+
 }]);
