99a926b617ba8e609deb500ca3af1fce0023aa3e
ameer dow <ameerdow@icloud.com>
2015-08-13 23:37:09 +0300

    globas

--- js/globals.js
+++ js/globals.js
@@ -1,2 +1,12 @@
-var CashedObjects = {};
-var CashedKbits = {};
+angular.module('IntelLearner', ['onsn','firebase']).factory('name', ['Globals', function(Globals){
+	
+	//Objects array
+	var CashedObjects = [];
+	//Kbits array
+	var CashedKbits = [];
+
+	function Globals(){
+		
+	};
+}])
+
