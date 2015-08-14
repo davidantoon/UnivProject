a294fd1107293bec8f25d8c8ff3b22667af7ba0c
ameer dow <ameerdow@icloud.com>
2015-08-14 15:52:52 +0300

    Storage save func fix

--- js/Storage.js
+++ js/Storage.js
@@ -27,40 +27,39 @@ app.factory('Storage', function(){
 				Done(false,"value is null");
 			break;
 			case 'number':
-				value = value.toString() + "||Number";
-				Done(true, value);
+				value = value.toString();
+				Done(true, value, "Number");
 			break;
 			case 'array':
-				value = value.toString() + "||Array";
-				Done(true,value);
+				value = value.toString();
+				Done(true, value,"Array");
 			break;
 			case 'string':
-				value += "||String";
-				Done(true, value);
+				Done(true, value,"String");
 			break;
 			case 'object':
 				if(TypeOf.get(value) == null || TypeOf.get(value) == undefined){
 						// Regular object 
-						value = JSON.stringify(value) + "||Object";
-						Done(true,value);
+						value = JSON.stringify(value);
+						Done(true, value, "Object");
 				}else{ // one of our objects (workspace or tab or content etc.)
 						switch(TypeOf.get(value)){
 							case Content.prototype.objectType:
-								value = Content.prototype.toString() + "||Content";
-								Done(true,value);
+								value = Content.prototype.toString();
+								Done(true, value,"Content");
 							break;
 							case Tab.prototype.objectType:
-								value = Tab.prototype.toString() + "||Tab";
-								Done(true,value);
+								value = Tab.prototype.toString() ;
+								Done(true, value,"Tab");
 							break;
 							case Workflow.prototype.objectType:
-								value = Workflow.prototype.toString() + "||Workflow";
-								Done(true,value);
+								value = Workflow.prototype.toString();
+								Done(true, value,"Workflow");
 							break;
 							default:
 							Done(false,"Error");
 							break;
-						}
+						} // end inner switch
 					}
 			break;
 			default:
@@ -68,11 +67,16 @@ app.factory('Storage', function(){
 			break;
 			} // end outter switch
 
-			function done(sucsess, data){
+			function done(sucsess, data, type){
 				if(sucsess){
-					localStorage.setItem(key, data);
+					objectSave = {
+						"data": data,
+						"type": type,
+						"lastModified": +(new Date);
+					}
+					localStorage.setItem(key, JSON.stringify(objectSave));
 				}else{
-					console.log(new Error(data));
+					console.log(new Error("Storage save() " + data));
 				}
 			}
 		}
