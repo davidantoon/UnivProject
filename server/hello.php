e747e1bd6e858ef18cb3129818d8d185f6b164de
antoon91 <antoon91@me.com>
2015-09-05 20:34:23 +0300

    fixing hello test

--- server/hello.php
+++ server/hello.php
@@ -179,6 +179,7 @@
 		debugLog::log("<i>[hello.php:function]</i> adding Scope: " . dbAPI::print_json_s($scopeG = scope::add_new_scope("scope title ". $i, "scope description ". $i, 0), 0));
 		array_push($scopeArr, $scopeG);
 		debugLog::log("<i>[hello.php:function]</i> adding term: " . dbAPI::print_json_s($termG = term::add_new_term_with_scope_and_meaning("term ". $i, "en", 0, $scopeArr[$i]["UID"], "term meaning ". $i), 0));
+		array_push($termArr, $termG);
 	}
 	
 	for($i=0; $i< 8; $i++) {
@@ -191,7 +192,7 @@
 
 			// creating relation of type K2T
 			$t = (7 - $i);
-			debugLog::log("<i>[hello.php:testing relation K2T (".$i."=>". $t .")]</i> result: ". dbAPI::print_json_s(Kbit::add_K2T_relation($kbitArr[$i]["UID"], $t, 'link type', 0), 0)); // succeeds
+			debugLog::log("<i>[hello.php:testing relation K2T (".$i."=>". $t .")]</i> result: ". dbAPI::print_json_s(Kbit::add_K2T_relation($kbitArr[$i]["UID"], $termArr[$t]["UID"], 'link type', 0), 0)); // succeeds
 		}
 	}
 
@@ -217,7 +218,7 @@
 			array_push($delArr, $deliveryG);
 			// creating relation of type K2T
 			$t = (7 - $i);
-			debugLog::log("<i>[hello.php:testing relation D2T (".$i."=>". $t .")]</i> result: ". dbAPI::print_json_s(Delivery::add_D2T_relation($delArr[$i]["UID"], $t, 'link type', 0), 0)); // succeeds
+			debugLog::log("<i>[hello.php:testing relation D2T (".$i."=>". $t .")]</i> result: ". dbAPI::print_json_s(Delivery::add_D2T_relation($delArr[$i]["UID"], $termArr[$t]["UID"], 'link type', 0), 0)); // succeeds
 	}
 
 
